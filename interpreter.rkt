4f5fc1ed000657139d701cd1ac89503c2638e588
Andrew Jones <me@andrewhjon.es>
2012-09-15 18:07:43 +0100

    Define uminus and make types work for f'n definition

--- interpreter.rkt
+++ interpreter.rkt
@@ -1,11 +1,11 @@
-#lang plai
+#lang plai-typed
 
 (define-type ExprC
-  [numC (n number?)]
-  [idC (s symbol?)]
-  [appC (fun symbol?) (arg ExprC?)]
-  [plusC (l ExprC?) (r ExprC?)]
-  [multC (l ExprC?) (r ExprC?)])
+  [numC (n : number)]
+  [idC (s : symbol)]
+  [appC (fun : symbol) (arg : ExprC)]
+  [plusC (l : ExprC) (r : ExprC)]
+  [multC (l : ExprC) (r : ExprC)])
 
 (define (interp e fds)
   (type-case ExprC e
@@ -20,7 +20,7 @@
     [multC (l r) (* (interp l fds) (interp r fds))]))
 
 (define-type FunDefC
-  [fdC (name symbol?) (arg symbol?) (body ExprC?)])
+  [fdC (name : symbol) (arg : symbol) (body : ExprC)])
 
 (define (get-fundef n fds)
   (cond
@@ -41,33 +41,40 @@
     [multC (l r) (multC (subst what for l)
                         (subst what for r))]))
 
-(define (parse e)
+(define (parse [e : s-expression])
   (cond
-    [(number? e) (numS e)]
-    [(list? e)
-     (case (first e)
-       [(+) (plusS (parse (second e)) (parse (third e)))]
-       [(*) (multS (parse (second e)) (parse (third e)))]
-       [(-) (bminusS (parse (second e)) (parse (third e)))])]))
+    [(s-exp-number? e) (numS (s-exp->number e))]
+    [(s-exp-list? e)
+     (let ([sl (s-exp->list e)])
+       (case (s-exp->symbol (first sl))
+         [(+) (plusS (parse (second sl)) (parse (third sl)))]
+         [(*) (multS (parse (second sl)) (parse (third sl)))]
+         [(-) (cond
+                [(empty? (rest (rest sl))) (uminusS (parse (second sl)))]
+                [else (bminusS (parse (second sl)) (parse (third sl)))])]
+         [else (error 'parse "invalid list input")]))]
+    [else (error 'parse "invalid input")]))
 
 (define-type ArithS
-  [numS (n number?)]
-  [plusS (l ArithS?) (r ArithS?)]
-  [bminusS (l ArithS?) (r ArithS?)]
-  [multS (l ArithS?) (r ArithS?)])
+  [numS (n : number)]
+  [plusS (l : ArithS) (r : ArithS)]
+  [bminusS (l : ArithS) (r : ArithS)]
+  [uminusS (e : ArithS)]
+  [multS (l : ArithS) (r : ArithS)])
 
 (define (desugar as)
   (type-case ArithS as
     [numS (n) (numC n)]
     [plusS (l r) (plusC (desugar l) (desugar r))]
     [multS (l r) (multC (desugar l) (desugar r))]
-    [bminusS (l r) (plusC (desugar l) (multC (numC -1) (desugar r)))]))
+    [bminusS (l r) (plusC (desugar l) (multC (numC -1) (desugar r)))]
+    [uminusS (n) (plusC (numC 0) (multC (numC -1) (desugar n)))]))
 
-(test 0 (interp (numC 0) '()))
-(test 1 (interp (plusC (numC 1) (numC 0)) '()))
-(test 6 (interp (multC (numC 2) (numC 3)) '()))
-(test 7 (interp (plusC (numC 1) (multC (numC 2) (numC 3))) '()))
-(test 2 (interp (desugar (parse '(- 4 2))) '()))
+(test 0 (interp (numC 0) (list)))
+(test 1 (interp (plusC (numC 1) (numC 0)) (list)))
+(test 6 (interp (multC (numC 2) (numC 3)) (list)))
+(test 7 (interp (plusC (numC 1) (multC (numC 2) (numC 3))) (list)))
+(test 2 (interp (desugar (parse '(- 4 2))) (list)))
 
   
 
